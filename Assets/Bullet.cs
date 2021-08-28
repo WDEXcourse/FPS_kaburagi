207942636a7fa76963239a8e42079ae872ef7c04
WDEXcourse <wdkawasaki.online@gmail.com>
2021-08-28 19:05:17 +0900

    Initial commit

---
+++ Assets/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    Target target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("Target").GetComponent<Target>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "Target")
+        {
+            target.Damage();
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Muzzle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Muzzle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject bullet;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKey(KeyCode.L))
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shot();
+        }
+    }
+
+    void Shot()
+    {
+        GameObject obj;
+        obj = GameObject.Instantiate(bullet);
+        obj.transform.position = transform.position;
+        obj.GetComponent<Rigidbody>().AddForce(transform.forward * 3000);
+
+        Destroy(obj, 1);
+    }
+
+    
+}
+
+
